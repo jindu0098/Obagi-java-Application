9d081b0f7aa020c4872b1bf1c3071516bed471bf
jindu0098 <prospertrx788@gmail.com>
2025-03-18 16:00:11 +0100

    print method

---
+++ PrintMethods.java
@@ -0,0 +1,21 @@
+public class PrintMethods{
+     public static void main(String[] args) {
+	 //Using the print()
+    System.out.print("\" Learning Java is fun\"\n") ;
+	System.out.print(" Hey, come and learn java with me") ; 
+	
+	//Using the println();
+	System.out.println(""); 
+	System.out.println("i want to tell you that java is different from java script") ;
+	System.out.println("\t this is indented text") ;
+	//Using the printf()
+	System.out.printf("My name is %s and i love %s%n","John","java") ;
+	System.out.printf(" i am living in NO. %d kaduna street%n",1) ; 
+	System.out.printf("i bought java JDK with %c%f Naira%n",'$',159.345) ;
+	System.out.printf("i bought java JDK with %.1f Naira",159.345) ;
+	
+	
+
+  }
+}
+  
